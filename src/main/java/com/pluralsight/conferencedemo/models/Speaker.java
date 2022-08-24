0dcb6dc9e23f62c7acdea9110ce8b7d2437088ac
hape <hapemartini@gmail.com>
2022-08-24 21:05:07 +0200

    add BLOB property for speaker photo

--- src/main/java/com/pluralsight/conferencedemo/models/Speaker.java
+++ src/main/java/com/pluralsight/conferencedemo/models/Speaker.java
@@ -1,5 +1,7 @@
 package com.pluralsight.conferencedemo.models;
 
+import org.hibernate.annotations.Type;
+
 import javax.persistence.*;
 import java.util.List;
 
@@ -13,6 +15,10 @@ public class Speaker {
     private String title;
     private String company;
     private String speaker_bio;
+
+    @Lob
+    @Type(type="org.hibernate.type.BinaryType")
+    private byte[] speaker_photo;
 
     @ManyToMany(mappedBy = "speakers")
     private List<Session> sessions;
